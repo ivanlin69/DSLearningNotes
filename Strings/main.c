b511987da68ad4d1966e20ea7f74fbd8427d40d0
ivanlin69 <117963772+ivanlin69@users.noreply.github.com>
2024-06-07 14:52:08 +0800

    Update main.c

--- Strings/main.c
+++ Strings/main.c
@@ -1,5 +1,54 @@
 #include <stdio.h>
 
+size_t Length(const char* str){
+    size_t i = 0;
+    for(; str[i]!='\0'; i++);
+    return i;
+}
+
+void Capitalized(char* str){
+    for(size_t i=0; str[i]!='\0'; i++){
+        if(str[i] >= 65 && str[i] <= 90){
+            str[i] -= 32;
+        }
+    }
+}
+
+void Uncaptalized(char* str){
+   for(size_t i=0; str[i]!='\0'; i++){
+       if(str[i] >= 97 && str[i] <= 122){
+           str[i] += 32;
+       }
+   }
+}
+
+void Toggle(char* str){
+   for(size_t i=0; str[i]!='\0'; i++){
+       if(str[i] >= 65 && str[i] <= 90){
+           str[i] += 32;
+       } else if(str[i] >= 97 && str[i] <= 122){
+           str[i] -= 32;
+       }
+   }
+}
+
+size_t CountWords(const char* str){
+    size_t count=0;
+    size_t word=0;
+    for(size_t i=0; str[i]!='\0'; i++){
+        if((str[i] >= 65 && str[i] <= 90) ||
+           (str[i] >= 97 && str[i] <= 122)){
+            if(word==0){
+                count++;
+                word=1;
+            }
+        } else{
+            word=0;
+        }
+    }
+    return count;
+}
+
 int main(int argc, const char * argv[]) {
     
     char a[100] = {'j', 'o', 'h', 'n', '\0'};
@@ -7,14 +56,22 @@ int main(int argc, const char * argv[]) {
     char c[] = "john";
     
     printf("%s %s %s \n", a, b, c);
-    scanf("%s", a);
-    printf("%s \n", a);
+    /**
+     scanf("%s", a);
+     printf("%s \n", a);
+     
+     // Consume the newline character left by scanf
+     char d = getchar();
+     while (d != '\n' && d != EOF);
+     
+     fgets(a, sizeof(a), stdin);
+     printf("%s \n", a);
+     */
     
-    // Consume the newline character left by scanf
-    char d = getchar();
-    while (d != '\n' && d != EOF);
+    char str[100] = "John   is   rich  .  ";
+    printf("%s\n", str);
     
-    fgets(a, sizeof(a), stdin);
-    printf("%s \n", a);
+    
+    
     return 0;
 }
