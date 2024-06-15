4b5f0eceeef00e886013fb52556e93b5d2f4462c
ivanlin69 <117963772+ivanlin69@users.noreply.github.com>
2024-06-15 22:23:57 +0800

    Add, Display function added

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -1,50 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 struct Node{
     int value;
     struct Node *next;
 };
 
-void AddNode(struct Node * head, int e){
+struct LinkedList{
+    size_t length;
+    struct Node *head;
+    struct Node *last;
+};
+
+void AddEnd(struct LinkedList * l, int e){
     
-    struct Node * temp = head;
-    
-    while(head->next != NULL){
-        head = head->next;
-    }
     struct Node * newNode = (struct Node *) malloc(sizeof(struct Node));
-    head->next = newNode;
     newNode->value = e;
     newNode->next = NULL;
-    head = temp;
+    
+    if(l->head == NULL){
+        l->head = newNode;
+        l->last = newNode;
+    } else {
+        l->last->next = newNode;
+        l->last = newNode;
+    }
+    l->length++;
 }
 
-void DisplayLL(struct Node * head){
-    struct Node * temp = head;
+void AddFront(struct LinkedList * l, int e){
     
-    while(head != NULL){
-        printf("%d ", head->value);
-        head = head->next;
+    struct Node * newNode = (struct Node *) malloc(sizeof(struct Node));
+    newNode->value = e;
+    newNode->next = l->head;
+    
+    if(l->head == NULL){
+        l->last = newNode;
+    }
+    l->head = newNode;
+    l->length++;
+}
+
+void DisplayLL(struct LinkedList * l){
+    struct Node * temp = l->head;
+    
+    while(temp != NULL){
+        printf("%d ", temp->value);
+        temp = temp->next;
     }
     printf("\n");
-    head = temp;
+}
+
+void DisplayLLRecursive(struct Node * head){
+    if(head != NULL){
+        printf("%d ", head->value);
+        DisplayLLRecursive(head->next);
+        // printf("%d ", head->value);  prints backward
+    }
 }
 
 int main(int argc, const char * argv[]) {
     
-    struct Node * LL = (struct Node *) malloc(sizeof(struct Node));
-    LL->value = 1;
-    LL->next = NULL;
+    struct LinkedList l = {0, NULL, NULL};
     
-    DisplayLL(LL);
+    DisplayLL(&l);
     
-    AddNode(LL, 34);
-    AddNode(LL, -3);
-    AddNode(LL, 386);
-    AddNode(LL, 67);
+    AddEnd(&l, 34);
+    AddEnd(&l, -3);
+    AddEnd(&l, 386);
+    AddEnd(&l, 67);
     
-    DisplayLL(LL);
+    AddFront(&l, 543);
+    
+    DisplayLL(&l);
+    DisplayLLRecursive(l.head);
+    printf("\n");
     
     return 0;
 }
