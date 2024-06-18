b43363979ea826f00afe42198a209176bf51ac53
ivanlin69 <117963772+ivanlin69@users.noreply.github.com>
2024-06-18 15:32:08 +0800

    LInkedlist v1 finished

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -260,6 +260,169 @@ int IsSorted(struct LinkedList * l){
     return 1;
 }
 
+void DeleteDuplicates(struct LinkedList * l){
+    
+    if(l->head == NULL || l->head->next == NULL){
+        return;
+    }
+    
+    struct Node * curr = l->head;
+    while(curr->next != NULL){
+        if(curr->next->value == curr->value){
+            struct Node * temp = curr->next;
+            if(curr->next->next == NULL){
+                l->tail = curr;
+            }
+            curr->next = curr->next->next;
+            free(temp);
+            l->length--;
+        } else{
+            // move forward if no duplictes
+            curr = curr->next;
+        }
+    }
+}
+
+void Reverse(struct LinkedList * l){
+    
+    if(l->head == NULL || l->head->next == NULL){
+        return;
+    }
+    struct Node * curr = l->head;
+    struct Node * next;
+    l->tail = curr;
+    
+    while(curr->next){
+        next = curr->next;
+        curr->next = next->next;
+        next->next = l->head;
+        l->head = next;
+    }
+}
+
+void ReverseV2(struct LinkedList * l){
+    
+    struct Node * r = NULL;
+    struct Node * q = NULL;
+    struct Node * p = l->head;
+    l->tail = l->head;
+    
+    while(p){
+        r = q;
+        q = p;
+        p = p->next;
+        q->next = r;
+    }
+    l->head = q;
+}
+
+void ReverseR(struct LinkedList * l, struct Node * n){
+    
+    if(n == NULL || n->next == NULL){
+        return;
+    }
+    
+    if(n->next->next != NULL){
+        ReverseR(l, n->next);
+    } else {
+        l->head = n->next;
+    }
+    
+    n->next->next = n;
+    n->next = NULL;
+    l->tail = n;
+}
+
+// doesn't keep the duplicates
+// merging without copying elements, note thay any modification on the elements affects all lists
+struct LinkedList * Merge2SortedLL(struct LinkedList * l1, struct LinkedList * l2){
+    
+    struct Node * h1 = l1->head;
+    struct Node * h2 = l2->head;
+    
+    if(l1->head == NULL){
+        return l2;
+    } else if(l2->head == NULL){
+        return l1;
+    }
+    
+    struct LinkedList * merged = (struct LinkedList *) malloc(sizeof(struct LinkedList));
+    struct Node * temp;
+    
+    merged->length = 0;
+    
+    if(h1->value > h2->value){
+        merged->head = h2;
+        h2 = h2->next;
+    } else if(h1->value < h2->value){
+        merged->head = h1;
+        h1 = h1->next;
+    } else{
+        merged->head = h1;
+        h1 = h1->next;
+        h2 = h2->next;
+    }
+    merged->length++;
+    temp = merged->head;
+    
+    while(h1 && h2){
+        if(h1->value > h2->value){
+            temp->next = h2;
+            h2 = h2->next;
+        } else if(h1->value < h2->value){
+            temp->next = h1;
+            h1 = h1->next;
+        } else{
+            temp->next = h1;
+            h1 = h1->next;
+            h2 = h2->next;
+        }
+        merged->length++;
+        temp = temp->next;
+    }
+    
+    while(h1){
+        temp->next = h1;
+        h1 = h1->next;
+        temp = temp->next;
+        merged->length++;
+    }
+    while(h2){
+        temp->next = h2;
+        h2 = h2->next;
+        temp = temp->next;
+        merged->length++;
+    }
+    
+    merged->tail = temp;
+    temp->next = NULL;
+    return merged;
+}
+
+int HaveLoop(struct LinkedList * l){
+    
+    struct Node * p = l->head;
+    struct Node * q = l->head;
+    
+    if(p == NULL || p->next == NULL){
+        return 0;
+    }
+    
+    while(p && q){
+        p = p->next;
+        q = q->next;
+        
+        if(q) {
+            // q moves faster
+            q = q->next;
+        }
+        
+        if(p == q){
+            return 1;
+        }
+    }
+    return 0;
+}
 
 
 int main(int argc, const char * argv[]) {
@@ -303,7 +466,7 @@ int main(int argc, const char * argv[]) {
     
     Insert(&l, 4, 666);
     DisplayLL(&l);
-     */
+
     
     struct LinkedList l2 = {0, NULL, NULL};
     
@@ -326,6 +489,61 @@ int main(int argc, const char * argv[]) {
     DisplayLL(&l2);
     
     printf("Is sorted? : %d\n", IsSorted(&l2));
+
+    
+    struct LinkedList l3 = {0, NULL, NULL};
+    
+    InsertSorted(&l3, 3);
+    InsertSorted(&l3, 3);
+    InsertSorted(&l3, 4);
+    InsertSorted(&l3, 9);
+    InsertSorted(&l3, 9);
+    InsertSorted(&l3, 9);
+    
+    DisplayLL(&l3);
+    
+    DeleteDuplicates(&l3);
+    InsertSorted(&l3, 16);
+    DisplayLL(&l3);
+    
+    Reverse(&l3);
+    DisplayLL(&l3);
+    
+    ReverseV2(&l3);
+    DisplayLL(&l3);
+    
+    ReverseR(&l3, l3.head);
+    DisplayLL(&l3);
+    */
+    
+    struct LinkedList l4 = {0, NULL, NULL};
+    
+    InsertSorted(&l4, 2);
+    InsertSorted(&l4, 3);
+    InsertSorted(&l4, 4);
+    InsertSorted(&l4, 9);
+    InsertSorted(&l4, 12);
+    InsertSorted(&l4, 14);
+    
+    DisplayLL(&l4);
+    
+    struct LinkedList l5 = {0, NULL, NULL};
+    
+    InsertSorted(&l5, 11);
+    InsertSorted(&l5, 14);
+    InsertSorted(&l5, 110);
+    InsertSorted(&l5, 119);
+    InsertSorted(&l5, 121);
+    
+    DisplayLL(&l5);
+    
+    struct LinkedList *merged = Merge2SortedLL(&l4, &l5);
+    DisplayLL(merged);
+    
+    // create a loop
+    merged->tail->next = merged->head->next;
+    printf("Is a loop? : %d \n", HaveLoop(merged));
+    
     
     return 0;
 }
