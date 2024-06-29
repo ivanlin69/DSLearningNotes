db843f961f91974ea395ad87043f872c43bd2323
ivanlin69 <117963772+ivanlin69@users.noreply.github.com>
2024-06-29 15:36:59 +0800

    Created BinaryTree

---
+++ BinaryTree/BinaryTree.h
@@ -0,0 +1,17 @@
+#ifndef BinaryTree_h
+#define BinaryTree_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    struct Node * left;
+    struct Node * right;
+    int value;
+};
+
+struct Tree{
+    struct Node * root;
+}
+
+#endif /* BinaryTree_h */
