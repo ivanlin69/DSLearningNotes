05c881bdfa0b6639064995c2abe23588195970e8
ivanlin69 <117963772+ivanlin69@users.noreply.github.com>
2024-07-01 22:16:39 +0800

    Updated structure

--- StackADTArray/main.c
+++ StackADTArray/main.c
@@ -1,67 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-struct Stack{
-    int * A;
-    int size;
-    int top;
-};
-
-void InitializeStack(struct Stack *s, int size){
-    
-    s->A = (int *) malloc(sizeof(int)*size);
-    s->size = size;
-    s->top = -1;
-}
-
-void Push(struct Stack *s, int e){
-    if(s->top < s->size-1){
-        s->A[++(s->top)] = e;
-        
-    } else {
-        printf("Stack is full.\n");
-    }
-}
-
-int Pop(struct Stack *s){
-    if(s->top < 0){
-        printf("Stack is empty.\n");
-        return -1;
-    }
-    return s->A[(s->top)--];
-}
-
-int Peek(const struct Stack *s, int index){
-    if(index <0 || index > s->top){
-        printf("Index out of bound.\n");
-        return -1;
-    }
-    return s->A[(s->top)-index];
-}
-
-int Top(const struct Stack *s){
-    if(s->top >= 0){
-        return s->A[s->top];
-    }
-    printf("Stack is empty.\n");
-    return -1;
-}
-
-int isEmpty(const struct Stack *s){
-    return s->top == -1;
-}
-
-int isFull(const struct Stack *s){
-    return s->top == s->size-1;
-}
-
-void FreeStack(struct Stack *s){
-    free(s->A);
-    s->A = NULL;
-    s->size = 0;
-    s->top = -1;
-}
+#include "Stack.h"
 
 
 int main(int argc, const char * argv[]) {
