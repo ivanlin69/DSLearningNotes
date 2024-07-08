3d462154481e896946e96dcf9d6550ac5c33ad49
ivanlin69 <117963772+ivanlin69@users.noreply.github.com>
2024-07-08 22:35:40 +0800

    Created AVL Tree

---
+++ AVLTree/AVLTree.h
@@ -0,0 +1,381 @@
+#ifndef AVLTree_h
+#define AVLTree_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "QueueLL.h"
+#include "Stack.h"
+
+
+struct AVLTree{
+    struct TreeNode * root;
+};
+
+//Note: The height of an empty tree is 0 and the height of a tree with single node is 1.
+int Height(struct TreeNode *t){
+    if(t != NULL){
+        int l = Height(t->left);
+        int r = Height(t->right);
+        if(l > r){
+            return l + 1;
+        }
+        return r + 1;
+    }
+    return 0;
+}
+
+struct TreeNode * LLRotation(struct TreeNode * t){
+    
+    return 
+}
+
+struct TreeNode * LRRotation(struct TreeNode * t){
+    
+}
+
+struct TreeNode * RRRotation(struct TreeNode * t){
+    
+}
+
+struct TreeNode * RLRotation(struct TreeNode * t){
+    
+}
+
+int HeightNode(struct TreeNode * t){
+    return Height(t->left) - Height(t->right);
+}
+
+int BalanceFactor(struct TreeNode * t){
+    return HeightNode(t->left) - HeightNode(t->right);
+}
+
+int InsertNode(struct AVLTree *b, int value){
+    if(b == NULL){
+        printf("Error: Invalid Tree.\n");
+        return -1;
+    }
+    
+    struct TreeNode * newNode = (struct TreeNode *) malloc(sizeof(struct TreeNode));
+    newNode->value = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    newNode->height = 1;
+    
+    if(b->root == NULL){
+        b->root = newNode;
+        return 0;
+    }
+    
+    struct TreeNode * temp = b->root;
+    struct TreeNode * prev = temp;
+    
+    while(temp){
+        if(value == temp->value){
+            printf("Error: Found duplicates.\n");
+            return -1;
+        } else if(value > temp->value){
+            prev = temp;
+            temp = temp->right;
+        } else{
+            prev = temp;
+            temp = temp->left;
+        }
+    }
+    
+    if(value > prev->value){
+        prev->right = newNode;
+    } else{
+        prev->left = newNode;
+    }
+    
+    if(BalanceFactor(b->root) == 2){
+        
+        if(BalanceFactor(b->root->left) == 1){
+            LLRotation(b->root);
+        } else {
+            LRRotation(b->root);
+        }
+    } else if(BalanceFactor(b->root) == -2){
+        
+        if(BalanceFactor(b->root->right) == -1){
+            RRRotation(b->root);
+        } else {
+            RLRotation(b->root);
+        }
+    }
+    
+    return 0;
+}
+
+
+struct TreeNode * InsertNodeR(struct TreeNode *t, int value){
+    if(t->right == NULL && value > t->value){
+        struct TreeNode * newNode = (struct TreeNode *) malloc(sizeof(struct TreeNode));
+        newNode->value = value;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        t->right = newNode;
+        return 0;
+    } else if(t->left == NULL && value < t->value){
+        struct TreeNode * newNode = (struct TreeNode *) malloc(sizeof(struct TreeNode));
+        newNode->value = value;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        t->left = newNode;
+        return 0;
+    }
+    
+    if(value > t->value){
+        return InsertNodeR2(t->right, value);
+    } else if(value < t->value){
+        return InsertNodeR2(t->left, value);
+    } else{
+        printf("Error: Found duplicates.\n");
+        return -1;
+    }
+}
+
+
+struct TreeNode * FindPredecessor(struct TreeNode * t){
+    struct TreeNode * temp = t->left;
+    while(temp->right){
+        temp = temp->right;
+    }
+    return temp;
+}
+
+struct TreeNode * FindSuccessor(struct TreeNode * t){
+    struct TreeNode * temp = t->right;
+    while(temp->left){
+        temp = temp->left;
+    }
+    return temp;
+}
+
+
+struct TreeNode * HelperDeleteNode(struct TreeNode *t, int value){
+    
+    if(!t){
+        return NULL;
+    }
+    
+    if(t->value == value){
+        
+        if(t->left == NULL){
+            struct TreeNode * temp = t->right;
+            free(t);
+            return temp;
+        } else if(t->right == NULL){
+            struct TreeNode * temp = t->left;
+            free(t);
+            return temp;
+        } else {
+            struct TreeNode * pred = NULL;
+            if(Height(t->left) > Height(t->right)){
+                pred = FindPredecessor(t);
+                t->value = pred->value;
+                t->left = HelperDeleteNode(t->left, pred->value);
+            } else{
+                pred = FindSuccessor(t);
+                t->value = pred->value;
+                t->right = HelperDeleteNode(t->right, pred->value);
+            }
+        }
+        
+    } else if(t->value > value){
+        t->left = HelperDeleteNode(t->left, value);
+    } else{
+        t->right = HelperDeleteNode(t->right, value);
+    }
+    return t;
+}
+
+int DeleteNode(struct AVLTree *b, int value){
+    
+    if(b == NULL || b->root == NULL){
+        printf("Error: Tree is empty.\n");
+        return -1;
+    }
+    b->root = HelperDeleteNode(b->root, value);
+    return 0;
+}
+
+
+
+void DisplayPreOrder(struct TreeNode *t){
+    if(t == NULL){
+        return;
+    }
+    printf("%d ", t->value);
+    DisplayPreOrder(t->left);
+    DisplayPreOrder(t->right);
+}
+
+void DisplayPreOrderIter(struct TreeNode *t){
+    
+    if(t == NULL){
+        return;
+    }
+    
+    struct Stack s;
+    InitializeStack(&s, 30);
+    
+    struct TreeNode * curr = t;
+    
+    while(curr != NULL || !isEmpty(&s)){
+        
+        if(curr != NULL){
+            Push(&s, curr);
+            printf("%d ", curr->value);
+            curr = curr->left;
+        } else{
+            curr = Pop(&s);
+            curr = curr->right;
+        }
+    }
+    FreeStack(&s);
+}
+
+void DisplayInOrder(struct TreeNode *t){
+    if(t == NULL){
+        return;
+    }
+    DisplayInOrder(t->left);
+    printf("%d ", t->value);
+    DisplayInOrder(t->right);
+}
+
+void DisplayInOrderIter(struct TreeNode *t){
+    
+    if(t == NULL){
+        return;
+    }
+    
+    struct Stack s;
+    InitializeStack(&s, 30);
+    
+    struct TreeNode * curr = t;
+    
+    while(curr != NULL || !isEmpty(&s)){
+        
+        if(curr != NULL){
+            Push(&s, curr);
+            curr = curr->left;
+        } else{
+            curr = Pop(&s);
+            printf("%d ", curr->value);
+            curr = curr->right;
+        }
+    }
+    FreeStack(&s);
+}
+
+void DisplayPostOrder(struct TreeNode *t){
+    if(t == NULL){
+        return;
+    }
+    DisplayPostOrder(t->left);
+    DisplayPostOrder(t->right);
+    printf("%d ", t->value);
+}
+
+void DisplayPostOrderIter(struct TreeNode *t){
+    
+    if(t == NULL){
+        return;
+    }
+    
+    struct Stack s1;
+    struct Stack s2;
+    InitializeStack(&s1, 30);
+    InitializeStack(&s2, 30);
+    
+    struct TreeNode * curr = t;
+    Push(&s1, curr);
+    
+    while(!isEmpty(&s1)){
+        curr = Pop(&s1);
+        Push(&s2, curr);
+        if(curr->left){
+            Push(&s1, curr->left);
+        }
+        if(curr->right){
+            Push(&s1, curr->right);
+        }
+    }
+    
+    while(!isEmpty(&s2)){
+        curr = Pop(&s2);
+        printf("%d ", curr->value);
+    }
+    FreeStack(&s1);
+    FreeStack(&s2);
+}
+
+void DisplayLevelOrder(struct TreeNode *t){
+    
+    if(t == NULL){
+        return;
+    }
+    
+    struct Queue q;
+    CreateQueue(&q);
+    struct TreeNode * curr = t;
+    EnQueue(&q, curr);
+    
+    while(!IsEmpty(&q)){
+        curr = DeQueue(&q);
+        printf("%d ", curr->value);
+        if(curr->left){
+            EnQueue(&q, curr->left);
+        }
+        if(curr->right){
+            EnQueue(&q, curr->right);
+        }
+    }
+    FreeQueue(&q);
+}
+
+int CountOfNodes(struct TreeNode *t){
+    int l, r;
+    if(t != NULL){
+        l = CountOfNodes(t->left);
+        r = CountOfNodes(t->right);
+        return l+r+1;
+    }
+    return 0;
+}
+
+int CountOfNodesDeg2(struct TreeNode *t){
+    if(t != NULL){
+        int l = CountOfNodesDeg2(t->left);
+        int r = CountOfNodesDeg2(t->right);
+        if(t->left && t->right){
+            return l+r+1;
+        }
+        return l+r;
+    }
+    return 0;
+}
+
+int CountLeafNodes(struct TreeNode *t){
+    if(t != NULL){
+        int l = CountOfNodesDeg2(t->left);
+        int r = CountOfNodesDeg2(t->right);
+        if(!t->left && !t->right){
+            return l+r+1;
+        }
+        return l+r;
+    }
+    return 0;
+}
+
+void FreeTree(struct TreeNode *t){
+    if(t != NULL){
+        FreeTree(t->left);
+        FreeTree(t->right);
+        free(t);
+    }
+}
+
+#endif /* AVLTree_h */
---
+++ AVLTree/LinkedList.h
@@ -0,0 +1,34 @@
+#ifndef LinkedList_h
+#define LinkedList_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode{
+    struct TreeNode * left;
+    struct TreeNode * right;
+    int height;
+    int value;
+};
+
+struct Node{
+    struct TreeNode *value;
+    struct Node *next;
+};
+
+struct LinkedList{
+    size_t length;
+    struct Node *head;
+    struct Node *tail;
+};
+
+void Initialize(struct LinkedList *l);
+int AddEnd(struct LinkedList * l, struct TreeNode * e);
+int AddFront(struct LinkedList * l, struct TreeNode * e);
+void DeleteFront(struct LinkedList * l);
+void Delete(struct LinkedList * l, size_t index);
+int Insert(struct LinkedList * l, size_t index, struct TreeNode * e);
+
+void freeLinkedlist(struct LinkedList *l);
+
+#endif /* LinkedList_h */
