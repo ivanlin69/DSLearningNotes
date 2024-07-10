c0cb324255239b37bfe78fc13148a01c9bff56c6
ivanlin69 <117963772+ivanlin69@users.noreply.github.com>
2024-07-10 22:18:35 +0800

    Created PriorityQueue

---
+++ PriorityQueue/PriorityQueue.h
@@ -0,0 +1,26 @@
+#ifndef PriorityQueue_h
+#define PriorityQueue_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct PriorityQueue{
+    int size;
+    int * A;
+    int length;
+};
+
+int insertPQ(struct PriorityQueue *p){
+    if(p == NULL){
+        printf("Invalid PriorityQueue.\n");
+        return -1;
+    }
+    
+    
+    
+    
+    
+    return 0;
+}
+
+#endif /* PriorityQueue_h */
