79897b064ab701a88f89e5fdf575fef5c7d38b84
ivanlin69 <117963772+ivanlin69@users.noreply.github.com>
2024-07-17 20:50:38 +0800

    Added create graph and BFS

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -3,6 +3,84 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "Stack.h"
+#include "QueueLL.h"
 
+struct AdjacencyList{
+    // an array of (pointers to) linkedlists
+    struct LinkedList **A;
+};
+
+struct Graph{
+    struct AdjacencyList * list;
+    size_t vertices;
+};
+
+void CreateGraph(struct Graph* g, size_t vertices){
+    g->vertices = vertices;
+    g->list = (struct AdjacencyList *) malloc(sizeof(struct AdjacencyList));
+    // Added extra 1 space for index 0
+    g->list->A = (struct LinkedList **) malloc(sizeof(struct LinkedList *)*(vertices+1));
+    for(size_t i=0; i<g->vertices+1; i++){
+        g->list->A[i] = (struct LinkedList *) malloc(sizeof(struct LinkedList));
+        Initialize(g->list->A[i]);
+    }
+}
+
+int AddEdge(struct Graph *g, int src, int des){
+    if(src > g->vertices || des > g->vertices){
+        printf("Vertex out of range.\n");
+        return -1;
+    }
+    InsertSorted(g->list->A[src], des);
+    //for undirected graph
+    InsertSorted(g->list->A[des], src);
+    return 0;
+}
+
+void BFS(struct Graph *g, int root){
+    if(g == NULL || root > g->vertices){
+        printf("Invalid graph or given vertex.\n");
+        return;
+    }
+    // arary for storing the visited vertices
+    int explored[g->vertices+1];
+    for(size_t i=0; i<=g->vertices; i++){
+        explored[i] = 0;
+    }
+    // queue for traversing the vertices in FIFO fashion
+    struct Queue q;
+    CreateQueue(&q);
+    EnQueue(&q, root);
+    printf("%d ", root);
+    explored[root] = 1;
+    
+    while(!IsEmpty(&q)){
+        int curr = DeQueue(&q);
+        struct Node * temp = g->list->A[curr]->head;
+        while(temp != NULL){
+            // if the node isn't visited
+            if(explored[temp->value] == 0){
+                printf("%d ", temp->value);
+                EnQueue(&q, temp->value);
+                // mark it as visited
+                explored[temp->value] = 1;
+            }
+            temp = temp->next;
+        }
+    }
+    printf("\n");
+    FreeQueue(&q);
+}
+
+void FreeGraph(struct Graph *g){
+    for(size_t i=0; i<=g->vertices; i++){
+        freeLinkedlist(g->list->A[i]);
+        free(g->list->A[i]);
+    }
+    free(g->list->A);
+    free(g->list);
+    g->vertices = 0;
+}
 
 #endif /* Graph_h */
--- Graph/main.c
+++ Graph/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include "Graph.h"
 
 int main(int argc, const char * argv[]) {
     
+    struct Graph g;
+    CreateGraph(&g, 7);
+    AddEdge(&g, 1, 2);
+    AddEdge(&g, 1, 3);
+    AddEdge(&g, 1, 4);
+    AddEdge(&g, 2, 3);
+    AddEdge(&g, 3, 4);
+    AddEdge(&g, 3, 5);
+    AddEdge(&g, 4, 5);
+    AddEdge(&g, 5, 6);
+    AddEdge(&g, 5, 7);
+    BFS(&g, 1);
     
-    
+    FreeGraph(&g);
     return 0;
 }
