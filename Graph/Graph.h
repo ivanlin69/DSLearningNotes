261e55cfadb7a2c0a766718113f86b4ae6e3d78e
ivanlin69 <117963772+ivanlin69@users.noreply.github.com>
2024-07-18 11:36:41 +0800

    Optimized structure

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -6,24 +6,20 @@
 #include "Stack.h"
 #include "QueueLL.h"
 
-struct AdjacencyList{
-    // an array of (pointers to) linkedlists
-    struct LinkedList **A;
-};
-
 struct Graph{
-    struct AdjacencyList * list;
+    // an array of (pointers to) linkedlists
+    struct LinkedList **AdjacencyList;
     size_t vertices;
 };
 
 void CreateGraph(struct Graph* g, size_t vertices){
     g->vertices = vertices;
-    g->list = (struct AdjacencyList *) malloc(sizeof(struct AdjacencyList));
     // Added extra 1 space for index 0
-    g->list->A = (struct LinkedList **) malloc(sizeof(struct LinkedList *)*(vertices+1));
+    g->AdjacencyList = (struct LinkedList **) malloc(sizeof(struct LinkedList *)*(vertices+1));
+    
     for(size_t i=0; i<g->vertices+1; i++){
-        g->list->A[i] = (struct LinkedList *) malloc(sizeof(struct LinkedList));
-        Initialize(g->list->A[i]);
+        g->AdjacencyList[i] = (struct LinkedList *) malloc(sizeof(struct LinkedList));
+        Initialize(g->AdjacencyList[i]);
     }
 }
 
@@ -32,9 +28,9 @@ int AddEdge(struct Graph *g, int src, int des){
         printf("Vertex out of range.\n");
         return -1;
     }
-    InsertSorted(g->list->A[src], des);
+    InsertSorted(g->AdjacencyList[src], des);
     //for undirected graph
-    InsertSorted(g->list->A[des], src);
+    InsertSorted(g->AdjacencyList[des], src);
     return 0;
 }
 
@@ -57,7 +53,7 @@ void BFS(struct Graph *g, int root){
     
     while(!IsEmpty(&q)){
         int curr = DeQueue(&q);
-        struct Node * temp = g->list->A[curr]->head;
+        struct Node * temp = g->AdjacencyList[curr]->head;
         while(temp != NULL){
             // if the node isn't visited
             if(explored[temp->value] == 0){
@@ -93,7 +89,7 @@ void DFS(struct Graph *g, int root){
     
     while(!isEmpty(&s)){
         int curr = Pop(&s);
-        temp = g->list->A[curr]->head;
+        temp = g->AdjacencyList[curr]->head;
         
         while(temp){
             while(temp && explored[temp->value] != 0){
@@ -104,7 +100,7 @@ void DFS(struct Graph *g, int root){
                 Push(&s, temp->value);
                 explored[temp->value] = 1;
                 
-                temp = g->list->A[temp->value]->head;
+                temp = g->AdjacencyList[temp->value]->head;
             }
         }
     }
@@ -135,7 +131,7 @@ void DFSv2(struct Graph *g, int root){
             explored[curr] = 1;
         }
         
-        temp = g->list->A[curr]->head;
+        temp = g->AdjacencyList[curr]->head;
         
         while(temp){
             if(explored[temp->value] == 0){
@@ -154,7 +150,7 @@ void HelperDFSr(struct Graph *g, int curr, int *explored){
     printf("%d ", curr);
     explored[curr] = 1;
     
-    struct Node * temp = g->list->A[curr]->head;
+    struct Node * temp = g->AdjacencyList[curr]->head;
     while(temp){
         if(explored[temp->value] == 0){
             HelperDFSr(g, temp->value, explored);
@@ -190,11 +186,10 @@ void DFSr2(struct Graph *g, int root){
 
 void FreeGraph(struct Graph *g){
     for(size_t i=0; i<=g->vertices; i++){
-        freeLinkedlist(g->list->A[i]);
-        free(g->list->A[i]);
+        freeLinkedlist(g->AdjacencyList[i]);
+        free(g->AdjacencyList[i]);
     }
-    free(g->list->A);
-    free(g->list);
+    free(g->AdjacencyList);
     g->vertices = 0;
 }
 
